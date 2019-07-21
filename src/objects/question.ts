3258d1aa405306112386654e27af83651200a8c0
Jeffrey <jc2573@cornell.edu>
2019-07-20 18:10:08 -0700

    Implement question blocks, start powerup

---
+++ src/objects/question.ts
@@ -0,0 +1,71 @@
+import { Coin } from "./coin"
+
+export class Question extends Phaser.GameObjects.Sprite {
+
+  private currentScene: Phaser.Scene;
+  body!: Phaser.Physics.Arcade.Body;
+  // private content: PowerUp;
+  private timeline: Phaser.Tweens.Timeline;
+  private opened: boolean;
+
+  constructor(params) {
+    super(params.scene, params.x, params.y, params.key, params.frame);
+    this.currentScene = params.scene;
+    this.initSprite();
+    this.opened = false;
+    this.timeline = this.currentScene.tweens.createTimeline({});
+    this.currentScene.add.existing(this);
+  }
+
+  private initSprite() {
+    this.setOrigin(0, 0);
+    this.currentScene.physics.world.enable(this);
+    this.body.setSize(16, 16);
+    this.body.setAllowGravity(false);
+    this.body.setImmovable(true);
+  }
+
+  update(): void {
+    if (!this.opened) {
+      this.anims.play("question", true);
+    }
+  }
+
+  boxHit(): void {
+    if (!this.opened) {
+      this.opened = true;
+      this.currentScene.sound.play("bump");
+      this.anims.stop();
+      this.setTexture("atlas", "question-empty");
+      this.timeline.add({
+        targets: this,
+        props: { y: this.y - 10 },
+        duration: 60,
+        ease: "Power0",
+        yoyo: true,
+      });
+      this.timeline.play();
+      this.currentScene.sound.play("coin");
+      let coin = new Coin({
+        scene: this.currentScene,
+        x: this.x + 2,
+        y: this.y - 20,
+        key: "atlas",
+        frame: "coin0"
+      });
+      coin.anims.play("coin", true);
+      this.currentScene.tweens.add({
+        targets: coin,
+        props: { y: this.y - 50 },
+        duration: 200,
+        ease: "Power0",
+        yoyo: true,
+        onComplete: () => { coin.destroy() }
+      });
+    }
+  }
+
+
+
+}
+
